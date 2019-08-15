147d3e018dc3f6f5f79942c8ebb76ea2923b2e93
Koichi Yoshigoe <yoshigoe@leapmind.io>
2019-08-16 02:46:24 +0900

    Update

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -15,39 +15,34 @@
   (python-mode-hook . jedi:setup)
   (python-mode-hook . jedi:ac-setup)
   (python-mode-hook . jedi:dot-complete)
+  (python-mode-hook . flymake-mode)
+  (python-mode-hook . flymake-python-pyflakes-load)
   :bind
   (:map python-mode-map
-        ("C-c j" . "jedi:complete")))
+        ("C-c j" . "jedi:complete"))
+  :config
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4)
+  (setq python-indent 4)
+  (setq c-basic-offset 4)
+  (setq c-basic-indent 4)
+  (autoload 'pylookup-lookup "pylookup")
+  (autoload 'pylookup-update "pylookup")
+  (setq py-shell-switch-buffers-on-execute-p t)
+  (setq py-switch-buffers-on-execute-p t)
+  (setq py-python-command-args
+        '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+  ;; don't split windows
+  (setq py-split-windows-on-execute-p nil)
+  ;; try to automagically figure out indentation
+  (setq py-smart-indentation t)
+  (setq py-force-py-shell-name-p t))
 
 (setq-default py-shell-name "ipython")
-(setq-default py-which-bufname "IPython")
-(setq py-python-command-args
-      '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq-default py-which-bufname "python")
 ;; switch to the interpreter after executing code
-(setq python-shell-interpreter "ipython"
-       python-shell-interpreter-args "-i")
-
-(add-hook 'python-mode-hook 'flymake-mode)
-(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-(add-hook 'python-mode-hook (lambda ()
-                              (setq indent-tabs-mode nil)
-                              (setq tab-width 4)
-                              (setq python-indent 4)
-                              (setq c-basic-offset 4)
-                              (setq c-basic-indent 4)
-                              (autoload 'pylookup-lookup "pylookup")
-                              (autoload 'pylookup-update "pylookup")))
-
-
-(setq py-shell-switch-buffers-on-execute-p t)
-(setq py-switch-buffers-on-execute-p t)
-
-;; don't split windows
-(setq py-split-windows-on-execute-p nil)
-
-;; try to automagically figure out indentation
-(setq py-smart-indentation t)
-(setq py-force-py-shell-name-p t)
+;; (setq python-shell-interpreter "ipython"
+;;        python-shell-interpreter-args "-i")
 
 ;; (use-package pip-requirements)
 (provide 'init-python)
