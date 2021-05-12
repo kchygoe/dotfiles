4a8e645e9fd852305ea6358fe8450ad96e4074fa
Koichi Yoshigoe <yoshigoe@leapmind.io>
2021-05-12 12:37:48 +0900

    update emacs dir

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,24 +1,16 @@
 ;;; init.el -*- lexical-binding: t; -*-
 ;;
-
-;; A big contributor to startup times is garbage collection. We up the gc
-;; threshold to temporarily prevent it from running, then reset it later by
-;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
-(setq gc-cons-threshold most-positive-fixnum)
-
-;; In noninteractive sessions, prioritize non-byte-compiled source files to
-;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
-;; to skip the mtime checks on every *.elc file.
-(setq load-prefer-newer noninteractive)
-
-(let (file-name-handler-alist)
-  ;; Ensure Doom is running out of this file's directory
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+;; native-comp
+;; (setq comp-speed 2)
 
 ;; Load the heart of Doom Emacs
 (setq doom-emacs-directory "~/src/github.com/hlissner/doom-emacs/")
 (load (concat doom-emacs-directory "core/core")
       nil 'nomessage)
+
+(unless (boundp 'doom-version)
+  (load (concat (file-name-directory load-file-name) "early-init")
+        nil t))
 
 ;; And let 'er rip!
 (doom-initialize)
